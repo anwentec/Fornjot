fc7318d4b4f36ec6dbcc22f3f71732258fe6ee00
Hanno Braun <hanno@braun-odw.eu>
2022-03-08 16:34:24 +0100

    Add cycle validation code

--- src/kernel/shape/cycles.rs
+++ src/kernel/shape/cycles.rs
@@ -2,11 +2,12 @@ use crate::kernel::topology::edges::Cycle;
 
 use super::{
     handle::{Handle, Storage},
-    CyclesInner,
+    CyclesInner, EdgesInner,
 };
 
 /// The cycles of a shape
 pub struct Cycles<'r> {
+    pub(super) edges: &'r mut EdgesInner,
     pub(super) cycles: &'r mut CyclesInner,
 }
 
@@ -21,6 +22,13 @@ impl Cycles<'_> {
     /// - That the cycle is not self-overlapping.
     /// - That there exists no duplicate cycle, with the same edges.
     pub fn add(&mut self, cycle: Cycle) -> Handle<Cycle> {
+        for edge in &cycle.edges {
+            assert!(
+                self.edges.contains(edge.storage()),
+                "Cycle validation failed: {edge:?} is not part of the shape",
+            );
+        }
+
         let storage = Storage::new(cycle);
         let handle = storage.handle();
         self.cycles.push(storage);
